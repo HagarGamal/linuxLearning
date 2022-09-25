e4fade4d88e8c06b5e88972553c5eaa2b1d0df63
Hagar Hemdan <hagar.gamal.halim@gmail.com>
2022-09-25 17:29:28 +0200

    implement the mv utility
    Signed-off-by: Hagar Hemdan <hagar.gamal.halim@gmail.com>

---
+++ assign2-FemtoUnixUtilities/mymv.c
@@ -0,0 +1,31 @@
+#include<unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<string.h>
+#include<stdio.h>
+int main(int argc, char ** argv)
+{
+  char buf[100];
+  int fd1 = open(argv[1], O_RDONLY, 0644);
+  int fd2 = open(argv[2], O_WRONLY| O_CREAT, 0644);
+
+  char * errorOpenMsg = "can not open the file\n";
+  if(fd1 == -1 || fd2 == -1)
+  {
+	write(1,errorOpenMsg, strlen(errorOpenMsg));
+	return -1;
+  }
+  else
+  {
+    int count;
+     while((count = read(fd1,buf, 100 )) !=0)
+     {
+       write(fd2, buf, count);
+     }
+  }
+  close(fd1);
+  close(fd2);
+  remove(argv[1]);
+}
+
