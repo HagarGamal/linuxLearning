651bed6735788842b73abb047928683fa3c17289
Hagar Hemdan <hagar.gamal.halim@gmail.com>
2022-10-09 16:10:46 +0200

    show the prompot msg as the bash
    Construct the prompot msg from the enviroment variables and handle the enter.
    
    Signed-off-by: Hagar Hemdan <hagar.gamal.halim@gmail.com>

--- assign3-FemtoShellExtension/femto_shell_extension.c
+++ assign3-FemtoShellExtension/femto_shell_extension.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+ #include <stdlib.h>
 int parse(char *s, char *strings[])
 {
     char *token;
@@ -19,30 +19,47 @@ int parse(char *s, char *strings[])
     return i;
 }
 
+const char * construct_promMsg()
+{
+    char * envUSER = getenv("USERNAME");
+    char * envPWD = getenv("PWD");
+    char * promMsg = envUSER;
+    strcat(promMsg, "@");
+    char hostname[100];
+    gethostname(hostname, 100);
+    strcat(promMsg, hostname);
+    strcat(promMsg, ":");
+    strcat(promMsg, envPWD);
+    strcat(promMsg, "$ ");
+    return promMsg;
+}
+
 int main()
 {
     char buff[100];
     int ret_pid;
     int status;
     char *strings[10];
+    const char * promMsg = construct_promMsg();
     while (1) {
-	printf("Ana Gahez ya basha > ");
+	printf("%s", promMsg);
 	fgets(buff, 100, stdin);
 	int len = strlen(buff);
 	buff[len - 1] = 0;
-	ret_pid = fork();
+	if (len > 1) {
+	    ret_pid = fork();
 
-	if (ret_pid == 0) {
-	    // child process
-	    parse(buff, strings);
-	    execvp(strings[0], strings);
-	} else if (ret_pid > 0) {
-	    // parent process
-	    wait(&status);
-	} else {
-	    printf("fork failed");
+	    if (ret_pid == 0) {
+		// child process
+		parse(buff, strings);
+		execvp(strings[0], strings);
+	    } else if (ret_pid > 0) {
+		// parent process
+		wait(&status);
+	    } else {
+		printf("fork failed");
+	    }
 	}
-
     }
     return 0;
 }
